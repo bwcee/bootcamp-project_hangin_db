6271d48b5cca1cc96cf5a3b21e689ac1a1cad93b
bw <bwceemail@gmail.com>
2022-03-21 20:20:21 +0800

    basic chats working - updated getUser() in userCtrl.mjs to also return email field - updated getPartnerTasks() in taskCtrl.mjs to also return pic field so can display avatar in chat - modified getAllTask() in taskCtrl.mjs to populate partner field w _id, name & pic so can display avatar in chat

--- socketEventHandlers/chatHandlers.mjs
+++ socketEventHandlers/chatHandlers.mjs
@@ -1,9 +1,22 @@
-export default function chatHandlers (io, socket) {
-  const testingConnx = (data) => {
-    return console.log(data.msg);
+let usersOnline = {};
+
+export default function chatHandlers(io, socket) {
+  const startChat = (data) => {
+    const { taskId, sender } = data;
+    /* join room so can subscribe to events emitted to this room, think this works cos ea socket is given a unique id */
+    socket.join(taskId);
+    /* saving sender id as obj in usersOnline since easier to add/locate/delete key-value pairs */
+    usersOnline[sender] = sender;
   };
 
-  socket.on("testing", testingConnx);
-};
+  const bdcastMsg = (data) => {
+    const { taskId } = data;
+    io.to(taskId).emit("bdcast_msg", data);
+  };
 
-
+  socket.on("start_chat", startChat);
+  socket.on("send_msg", bdcastMsg);
+  socket.on("disconnect", () => {
+    usersOnline = {};
+  });
+}
